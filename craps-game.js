dac0c03cfe301b57c0ac3a01305d3a9798183912
Tim Kennedy <tim.kennedy6@yahoo.com>
2025-07-29 16:22:59 -0700

    second commit

--- craps-game.js
+++ craps-game.js
@@ -1,6 +1,12 @@
 let yourPoint;
+let isRollingPhase = false;
 
 function comeOutRoll() {
+    if (isRollingPhase){
+        alert('You already have a point. Keep rolling.');
+        return;
+    }
+
     let roll = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
     alert(`You rolled a ${roll}`);
     if ((roll === 2) || (roll === 3) || (roll === 12)) {
@@ -12,18 +18,26 @@ function comeOutRoll() {
     } 
     else {
         yourPoint = roll;
+        isRollingPhase = true;
     }
 }
 
 function rollDice() {
+    if (!isRollingPhase){
+        alert('You need to establish a point first.');
+        return;
+    }
+
     let roll = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
     if (roll === 7) {
         alert(`You rolled a ${roll}, you crapped out`);
+        isRollingPhase = false;
         return;
     } else if (roll != yourPoint) {
         alert(`You rolled a ${roll}, roll again.`);
     } else {
         alert(`you rolled ${yourPoint}, you win!`);
+        isRollingPhase = false;
         return;
     }
 }
